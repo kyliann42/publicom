ff48de286749a0312e3e16540da8401baab03f23
GalaxyJay <borcedre@gmail.com>
2025-09-25 11:43:06 +0200

    modfication dans la vue ajout_message

--- app/Views/ajout_message.php
+++ app/Views/ajout_message.php
@@ -10,18 +10,27 @@
 </head>
 <body>
     <form method="post" action="#">
-        <label>Message :</label>
-        <textarea name="message">
-        </textarea>
-        <label> :</label>
-        <input name="titre" type="text">
-        <input name="policeTitre" type="text">
-        <input name="policeTexte" type="text">
-        <input name="alignement" type="text">
-        <input name="tailleTitre" type="text">
-        <input name="tailleTexte" type="text">
-        <input name="fond" type="text">
-        <input type="submit" value="Valider">
+       <fieldset> 
+            <legend>Ajout de message de {nom de la commune}</legend>
+            <label>Message :</label>
+            <textarea name="message">
+            </textarea>
+            <label>Titre :</label>
+            <input name="titre" type="text">
+            <label>Police de caractères du titre :</label>
+            <input name="policeTitre" type="text">
+            <label>Police de caractères du texte :</label>
+            <input name="policeTexte" type="text">
+            <label>Alignement du texte :</label>
+            <input name="alignement" type="text">
+            <label>Taille du titre :</label>
+            <input name="tailleTitre" type="text">
+            <label>Taille du texte :</label>
+            <input name="tailleTexte" type="text">
+            <label>Fond :</label>
+            <input name="fond" type="text">
+            <input type="submit" value="Valider">
+        </fieldset>
     </form>
 </body>
 </html>
