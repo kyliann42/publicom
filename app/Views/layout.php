eb282c2cb37695f61208b42766e23194a5016a7f
GalaxyJay <borcedre@gmail.com>
2025-10-02 10:18:49 +0200

    ajout d'un menu au layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -5,9 +5,18 @@
     <title>Publicom</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+     <link rel="stylesheet" href="css/menu.css">
     <?= $this->renderSection('css')?>
 </head>
 <body>
+<nav>
+    <ul class="main-nav">
+        <li><a href=""> Liste panneaux</a></li>
+        <li><a href="">Liste messages</a></li>
+        <li><a href="">Liste utilisateurs</a></li>
+        <li class="push"><a href="">Sortir de la commune</a></li>
+    </ul>
+    </nav>
   <?= $this->renderSection('contenu')?>
 </body>
 </html>
